bdaa42c42a5d549815a87eaccd01957caf1b14d8
Davyd Kuleba <kulebadavid@gmail.com>
2023-03-21 22:13:15 -0400

    Initial Commit

---
+++ 9 - lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9 - lesson
+//
+//  Created by Давид Кулеба on 2023-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
